fdbaf118af97049f2658e4898c00eec7710a7c0e
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-11-27 21:15:20 -0300

    exercícios 1 ao 4

--- exercicios.py
+++ exercicios.py
@@ -1,1 +1,8 @@
 # Exercício 01
+# print([int(x)**2 for x in input("Entre com os números separados por espaço: ").split()])
+# Exercício 02
+# print([x if x >= 10 else 0 for x in [int(i) for i in input("Entre com os números separados por espaço: ").split()]])
+# Exercício 03
+# print([len(x.split()) for x in input("Entre com a sua história: ").split(". ")])
+# Exercício 04
+print([sum(1 for c in texto if c.lower() in "aáâãàeêéèiîíìoôóõòuúûù") for texto in ["Frase um", "Frase dois", "Frase três"]])
