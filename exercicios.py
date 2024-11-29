2f2ea0a749ee69957e64b09a90853b05a092a5a5
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-11-29 09:39:32 -0300

    exercicio 05

--- exercicios.py
+++ exercicios.py
@@ -5,4 +5,17 @@
 # Exercício 03
 # print([len(x.split()) for x in input("Entre com a sua história: ").split(". ")])
 # Exercício 04
-print([sum(1 for c in texto if c.lower() in "aáâãàeêéèiîíìoôóõòuúûù") for texto in ["Frase um", "Frase dois", "Frase três"]])
+# print([sum(1 for c in texto if c.lower() in "aáâãàeêéèiîíìoôóõòuúûù") for texto in ["Frase um", "Frase dois", "Frase três"]])
+# Exercício 05
+maioresDeIdade = lambda lista:list(filter(lambda x:x["idade"]>=18,lista))
+pessoas = [
+    {"nome": "João", "idade": 25},
+    {"nome": "Maria", "idade": 17},
+    {"nome": "Pedro", "idade": 34},
+    {"nome": "Ana", "idade": 16},
+    {"nome": "Lucas", "idade": 18},
+    {"nome": "Clara", "idade": 15},
+    {"nome": "Gabriel", "idade": 22},
+    {"nome": "Bianca", "idade": 14}
+]
+print(maioresDeIdade(pessoas))
